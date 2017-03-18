8fe0c81f15164371bf9a416d733d46f40e6903ac
ilackarms <sdw35@cornell.edu>
2017-03-17 23:30:42 -0400

    get all users test (with no query and basic query)

--- models/api/v1/user_test.go
+++ models/api/v1/user_test.go
@@ -94,5 +94,68 @@ var _ = Describe("User", func() {
 				})
 			})
 		})
+		FDescribe("GetAllUser", func() {
+			var (
+				query  map[string]string
+				fields []string
+				sortBy []string
+				order  []string
+				offset int64
+				limit  int64
+
+				user1, user2, user3 *User
+			)
+			BeforeEach(func() {
+				query = make(map[string]string)
+
+				user1 = createAndAddUser("testuser1", "testpassword")
+				user2 = createAndAddUser("testuser2", "testpassword")
+				user3 = createAndAddUser("testuser3", "testpassword")
+			})
+			Context("empty query", func() {
+				It("returns all matches", func() {
+					assertExpectedUsers(query, fields, sortBy, order, offset, limit,
+						user, user1, user2, user3)
+				})
+			})
+			Context("set query[username] = user.Username", func() {
+				BeforeEach(func() {
+					query["username"] = user.Username
+				})
+				It("returns a list with only that user", func() {
+					assertExpectedUsers(query, fields, sortBy, order, offset, limit,
+						user)
+				})
+			})
+		})
 	})
 })
+
+func createAndAddUser(username, password string) *User {
+	user, err := NewUser(username, password)
+	testhelpers.Must(err)
+	id, err := AddUser(user)
+	testhelpers.Must(err)
+	user.ID = id
+	return user
+}
+
+func assertExpectedUsers(query map[string]string, fields []string, sortBy []string, order []string,
+	offset int64, limit int64, expUsers ...*User) {
+	users, err := GetAllUser(query, fields, sortBy, order, offset, limit)
+	Expect(err).NotTo(HaveOccurred())
+	var expectedUsers []*User
+	for _, user := range expUsers {
+		expectedUsers = append(expectedUsers, user)
+	}
+	Expect(users).To(HaveLen(len(expectedUsers)))
+	for i, item := range users {
+		user := item.(User)
+		expectedUser := expectedUsers[i]
+		Expect(user.ID).To(Equal(expectedUser.ID))
+		Expect(user.Username).To(Equal(expectedUser.Username))
+		Expect(user.PasswordHash).To(Equal(expectedUser.PasswordHash))
+		Expect(user.PasswordSalt).To(Equal(expectedUser.PasswordSalt))
+		Expect(user.Created.Equal(expectedUser.Created)).To(BeTrue())
+	}
+}
