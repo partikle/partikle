6f861aabed2f12651c6c347b3bf2d8fb647e7df2
ilackarms <sdw35@cornell.edu>
2017-03-17 22:41:43 -0400

    added add test, init db test helpers

--- models/api/v1/user_test.go
+++ models/api/v1/user_test.go
@@ -5,13 +5,23 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/partikle/partikle/testhelpers"
 )
+
+func init() {
+}
 
 var _ = Describe("User", func() {
 	var (
 		user *User
 		err  error
 	)
+	BeforeSuite(func() {
+		testhelpers.Must(testhelpers.InitTestDB())
+	})
+	AfterSuite(func() {
+		testhelpers.Must(testhelpers.DestroyTestDB())
+	})
 	BeforeEach(func() {
 		user, err = NewUser("testuser", "testpassword")
 	})
@@ -20,6 +30,15 @@ var _ = Describe("User", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(user.PasswordSalt).NotTo(BeEmpty())
 			Expect(user.PasswordHash).NotTo(Equal("testpassword"))
+		})
+	})
+	Describe("AddUser", func() {
+		Context("user has not been added yet", func() {
+			It("returns the user id without error", func() {
+				id, err := AddUser(user)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(id).To(Equal(int64(1)))
+			})
 		})
 	})
 })
---
+++ testhelpers/test_helpers.go
@@ -0,0 +1,31 @@
+package testhelpers
+
+import (
+	"github.com/astaxie/beego/orm"
+	"github.com/ilackarms/pkg/errors"
+	_ "github.com/mattn/go-sqlite3" // import your required driver
+	"os"
+)
+
+func InitTestDB() error {
+	if err := orm.RegisterDataBase("default", "sqlite3", "test_data.db"); err != nil {
+		return errors.New("registering to sqlite db", err)
+	}
+	if err := orm.RunSyncdb("default", true, true); err != nil {
+		return errors.New("syncing db", err)
+	}
+	return nil
+}
+
+func DestroyTestDB() error {
+	if err := os.Remove("test_data.db"); err != nil {
+		return errors.New("cleaning up test_data.db", err)
+	}
+	return nil
+}
+
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
